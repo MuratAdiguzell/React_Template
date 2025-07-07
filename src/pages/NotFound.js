56b0f8a05d9121db704e81d34e6f5262f00b8b36
MuratAdiguzell <mur-at-adgzl@hotmail.com>
2025-07-07 12:41:34 +0300

    fix(Not Found Page):Dil değişikliği yapıldı

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,9 +6,9 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>Page Not Found</h2>
+      <h2>Sayfa Bulunamadi.</h2>
       <p>The page you are looking for does not exist or may have moved.</p>
-      <Link to="/" className="home-link">Return to Home Page</Link>
+      <Link to="/" className="home-link">Anasayfaya Dön</Link> 
     </div>
   );
 };
